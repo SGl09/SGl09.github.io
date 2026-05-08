668b42e5228cf7a0491a26631ad7c05fb85b6219
SGl09 <thebackseatboiz@gmail.com>
2026-05-08 14:06:02 -0500

    saffsaf

---
+++ JavaScript Stuff/COOL LOGIN SCRN/script.js
@@ -0,0 +1,52 @@
+here = document.getElementById("here");
+function changeText() {
+    number = Math.floor(Math.random()* 100);
+    
+
+
+
+
+
+
+    if (number < 10) {
+        here.innerHTML = "nope";
+        document.getElementById("log").style.marginLeft ="0px"
+        document.getElementById("log").style.marginTop ="4px"
+    }
+    else if (number < 20) {
+        here.innerHTML = "not happening";
+        document.getElementById("log").style.marginLeft ="0px"
+        document.getElementById("log").style.marginTop ="4px"
+    }
+    else if (number < 30) {
+        here.innerHTML = "still not lettin u in";
+        document.getElementById("log").style.marginLeft ="0px"
+        document.getElementById("log").style.marginTop ="4px"
+    }
+    else if (number < 40) {
+        here.innerHTML = "gtfo";
+        document.getElementById("log").style.marginLeft ="0px"
+        document.getElementById("log").style.marginTop ="4px"
+    }
+    else if (number < 50) {
+        here.innerHTML = "oops, moved the button. GO GET IT!!!!";
+        document.getElementById("log").style.marginLeft ="500px"
+        document.getElementById("log").style.marginTop ="4px"
+    }
+    else if (number < 60) {
+        here.innerHTML = "oooooohhhhh wonder where it went? maybe it felldown.";
+        document.getElementById("log").style.marginTop ="1000px"
+    }
+}
+here = document.getElementById("here");
+username = document.getElementById("username");
+password = document.getElementById("type-here");
+function correctUNPW() {
+    if (username < innerHTML.sloppykissesfromgrandma, password < innerHML.babybaby123)
+        here.innerHTML = "YAY YOU DID IT!!!!!!!!!"
+
+
+}
+
+document.getElementById("log").addEventListener("click", changeText);
+document.getElementById("log").addEventListener("click", correctUNPW);
--- JavaScript Stuff/LootBox/script.js
+++ JavaScript Stuff/LootBox/script.js
@@ -1,48 +1,91 @@
 lootbox = document.getElementById("lootbox")
 function  rollLootbox(){
     number = Math.floor(Math.random()* 100)
+var common = document.getElementById("common");
+var c_count = 0;
+
+var uncommon = document.getElementById("uncommon");
+var u_count = 0;
+
+var rare = document.getElementById("rare");
+var r_count = 0;
+
+var epic = document.getElementById("epic");
+var e_count = 0;
+
+var legendary = document.getElementById("legendary");
+var l_count = 0;
+
+var mythic = document.getElementById("mythic");
+var m_count = 0;
+
+var wow = document.getElementById("wow");
+var w_count = 0;
+
+var impossible = document.getElementById("impossible");
+var im_count = 0;
+
+
+
 
 
 
     if (number < 50) {
         lootbox.innerHTML = "COMMON";
         lootbox.style.backgroundColor = "gray"
+        c_count = c_count + 1;
+        common.innerHTML = c_count;
     }
 
     else if (number < 75) {
         lootbox.innerHTML = "UNCOMMON";
         lootbox.style.backgroundColor = "green"
+        u_count = u_count + 1;
+        uncommon.innerHTML = u_count;
     }
 
     else if (number < 85) {
         lootbox.innerHTML = "RARE";
         lootbox.style.backgroundColor = "blue"
+        r_count = r_count + 1;
+        rare.innerHTML = r_count;
     }
 
     else if (number < 95) {
         lootbox.innerHTML = "EPIC";
         lootbox.style.backgroundColor = "purple"
+        e_count = e_count + 1;
+        epic.innerHTML = e_count;
     }
 
     else if (number < 99) {
         lootbox.innerHTML = "LEGENDARY";
         lootbox.style.backgroundColor = "gold"
+        l_count = l_count + 1;
+        legendary.innerHTML = l_count;
     }
 
     else if (number < 99.5) {
         lootbox.innerHTML = "MYTHIC";
         lootbox.style.backgroundColor = "cyan"
+        m_count = m_count + 1;
+        mythic.innerHTML = m_count;
     }
 
     else if (number < 99.75) {
         lootbox.innerHTML = "WOW";
         lootbox.style.backgroundColor = "pink"
+        w_count = w_count + 1;
+        wow.innerHTML = w_count;
     }  
 
     else if (number < 99.99) {
         lootbox.innerHTML = "IMPOSSIBLE";
         lootbox.style.backgroundColor = "MidnightBlue"
+        im_count = im_count + 1;
+        impossible.innerHTML = im_count;
     }    
 }
 
 document.getElementById("roll").addEventListener("click", rollLootbox);
+document.getElementById("username").value
