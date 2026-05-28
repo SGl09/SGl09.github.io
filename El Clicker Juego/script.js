084d89a4456f979676cf8f03f442bbcaffe29f92
SGl09 <thebackseatboiz@gmail.com>
2026-05-28 14:09:18 -0500

    tdgdrfg

--- El Clicker Juego/script.js
+++ El Clicker Juego/script.js
@@ -1,8 +1,12 @@
 var itemMonster = document.getElementById("itemMonster")
 var counter = document.getElementById("counterMonster")
+var upgrade = document.getElementById("clicker-upgrade-B")
+var upgrade2 = document.getElementById("autoclicker-upgrade-B")
 
 var numItem = 0
 var itemPerClick = 1
+var autoEarn = 0
+
 
 counter.innerHTML = "0 Monsters";
 
@@ -11,6 +15,29 @@ function clickItem(){
     counter.innerHTML = numItem.toString() + " Monsters";
 }
 
+function upgradeClick(){
+    if (numItem > 14) {
+        itemPerClick = itemPerClick + 1
+        numItem = numItem - 15
+        counter.innerHTML = numItem.toString() + " Monsters";
+        upgrade.innerHTML = "Upgrade Click: 500 monsters"
+    }
+}
+function autoClick(){
+    if (numItem > 199) {
+        autoEarn = autoEarn + 1
+        numItem = numItem - 200
+        counter.innerHTML = numItem.toString() + " Monsters";
+        upgrade2.innerHTML = "Autoclick: 1500 monsters"
+        
+    }
+}
+function autoClickAction(){
+    numItem = numItem + autoEarn;
+    counter.innerHTML = numItem.toString() + " Monsters";
 
+}
 
 itemMonster.addEventListener("click", clickItem)
+upgrade.addEventListener("click", upgradeClick)
+upgrade2.addEventListener("click", autoClick)
